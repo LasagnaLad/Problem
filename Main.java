adf6a8437665bc9fa375ce07dcf3c8d0f7f8fc19
Mr. Moeller <alexmoeller88@gmail.com>
2020-07-31 16:51:59 +0000

    I solved project euler problem number 1

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //This solves a problem
+    int counter = 0;
+    for(int x = 1; x < 10; x++){
+      if(x % 3 == 0 || x % 5 == 0){
+        counter -= -x;
+      }
+    }
+    System.out.println("The sum of all numbers that are divisible by 3 and 5 and under 10 is " + counter);
   }
 }
